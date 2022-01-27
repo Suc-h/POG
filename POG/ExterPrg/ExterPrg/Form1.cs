77b9eedc79e28c2958ad9a8c886a5e4752774222
Suc-h <97514706+Suc-h@users.noreply.github.com>
2022-01-27 16:19:00 +0100

    Oprava klonování

---
+++ POG/ExterPrg/ExterPrg/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExterPrg
+{
+    public partial class Form1 : Form
+    {
+        int n,cifry,cifsoucet;
+        string cifsouc;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            n = Convert.ToInt32(numericUpDown1.Value);
+            int help = n;
+            while (n > 0)
+            {
+                cifry = n % 10;
+                cifsoucet = cifsoucet + cifry;
+                n = n / 10;
+            }
+            n = help;
+           
+
+            for (int i = 1; i <= n; i++)
+            {
+                if (n % i == 0)
+                {
+                   Delitele.Items.Add(i.ToString());
+                }
+                
+            }  
+            cifsouc = Convert.ToString(cifsoucet);
+            MessageBox.Show(cifsouc);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Delitele.Items.Clear();
+            numericUpDown1.ResetText();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
